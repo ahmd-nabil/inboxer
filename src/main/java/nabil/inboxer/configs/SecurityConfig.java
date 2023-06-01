08de6c6cfa42b35c128090f5a16f9ddfc622a242
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-01 13:21:26 +0300

    first commit 'with oauth github login'

---
+++ src/main/java/nabil/inboxer/configs/SecurityConfig.java
@@ -0,0 +1,35 @@
+package nabil.inboxer.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                ).oauth2Login(Customizer.withDefaults());
+       return http.build();
+    }
+}
---
+++ src/test/java/nabil/inboxer/InboxerApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.inboxer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
