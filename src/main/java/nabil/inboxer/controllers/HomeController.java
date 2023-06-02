986894742cc30fc350b72e916c34c254ebc95893
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-06-02 22:28:46 +0300

    extracting mainService to prevent code duplicate(I hate thymeleaf)

--- src/main/java/nabil/inboxer/controllers/HomeController.java
+++ src/main/java/nabil/inboxer/controllers/HomeController.java
@@ -1,22 +1,14 @@
 package nabil.inboxer.controllers;
 
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 import lombok.RequiredArgsConstructor;
-import nabil.inboxer.email_list.EmailListItem;
-import nabil.inboxer.email_list.EmailListItemRepository;
-import nabil.inboxer.folders.Folder;
-import nabil.inboxer.folders.FolderRepository;
-import org.ocpsoft.prettytime.PrettyTime;
+import nabil.inboxer.services.MainService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Ahmed Nabil
@@ -25,36 +17,21 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final FolderRepository folderRepository;
-    private final EmailListItemRepository emailListItemRepository;
+    private final MainService mainService;
 
     @GetMapping("/")
-    public String homePage(Model model, @AuthenticationPrincipal OAuth2User principal) {
+    public String homePage(Model model, @AuthenticationPrincipal OAuth2User principal, @RequestParam(defaultValue = "inbox") String label) {
         System.out.println(principal);
         if(principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
             return "index";
         }
         String userName = principal.getAttribute("name");
         String userId = principal.getAttribute("login");
-        fetchUserFolders(model, userName, userId);
-        fetchUserEmailListItems(model, userId);
+        mainService.addUserFoldersToModel(model, userName, userId);
+        mainService.addEmailListToModel(model, userId, label);
         return "home";
     }
 
-    private void fetchUserEmailListItems(Model model, String userId) {
-        List<EmailListItem> emailListItems = emailListItemRepository.findAllByKey_UserIdAndKey_Label(userId, "sent");
-        emailListItems.forEach(item -> {
-            UUID timeUUID = item.getKey().getCreatedAt();
-            Date createdDate = new Date(Uuids.unixTimestamp(timeUUID));
-            item.setTimeAgo(new PrettyTime().format(createdDate));
-        });
-        model.addAttribute("emailItems", emailListItems);
-    }
 
-    private void fetchUserFolders(Model model, String userName, String userId) {
-        List<Folder> userFolders = folderRepository.findAllByUserId(userId);
-        model.addAttribute("userName", userName);
-        model.addAttribute("userFolders", userFolders);
-    }
 
 }
---
+++ src/main/java/nabil/inboxer/services/MainService.java
@@ -0,0 +1,43 @@
+package nabil.inboxer.services;
+
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import lombok.RequiredArgsConstructor;
+import nabil.inboxer.email_list.EmailListItem;
+import nabil.inboxer.email_list.EmailListItemRepository;
+import nabil.inboxer.folders.Folder;
+import nabil.inboxer.folders.FolderRepository;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Service
+@RequiredArgsConstructor
+public class MainService {
+
+    private final FolderRepository folderRepository;
+    private final EmailListItemRepository emailListItemRepository;
+
+    public void addEmailListToModel(Model model, String userId, String label) {
+        List<EmailListItem> emailListItems = emailListItemRepository.findAllByKey_UserIdAndKey_Label(userId, label);
+        emailListItems.forEach(item -> {
+            UUID timeUUID = item.getKey().getCreatedAt();
+            Date createdDate = new Date(Uuids.unixTimestamp(timeUUID));
+            item.setTimeAgo(new PrettyTime().format(createdDate));
+        });
+        model.addAttribute("emailItems", emailListItems);
+    }
+
+    public void addUserFoldersToModel(Model model, String userName, String userId) {
+        List<Folder> userFolders = folderRepository.findAllByUserId(userId);
+        model.addAttribute("userName", userName);
+        model.addAttribute("userFolders", userFolders);
+    }
+
+}
